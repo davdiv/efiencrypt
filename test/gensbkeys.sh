7e560cc73b80143242b2c4795de9daf68c1b8e1a
DivDE <divde@musicociel.fr>
2026-01-08 12:19:54 +0100

    Remove unused generated files

--- test/gensbkeys.sh
+++ test/gensbkeys.sh
@@ -6,7 +6,6 @@ set -e
 # Platform key:
 uuidgen --random > GUID.txt
 openssl req -newkey rsa:4096 -nodes -keyout PK.key -new -x509 -sha256 -days 365242 -subj "/CN=Platform Key/" -out PK.crt
-openssl x509 -outform DER -in PK.crt -out PK.cer
 cert-to-efi-sig-list -g "$(< GUID.txt)" PK.crt PK.esl
 sign-efi-sig-list -g "$(< GUID.txt)" -k PK.key -c PK.crt PK PK.esl PK.auth
 
@@ -15,12 +14,10 @@ sign-efi-sig-list -g "$(< GUID.txt)" -c PK.crt -k PK.key PK /dev/null noPK.auth
 
 # Key Exchange Key:
 openssl req -newkey rsa:4096 -nodes -keyout KEK.key -new -x509 -sha256 -days 365242 -subj "/CN=Key Exchange Key/" -out KEK.crt
-openssl x509 -outform DER -in KEK.crt -out KEK.cer
 cert-to-efi-sig-list -g "$(< GUID.txt)" KEK.crt KEK.esl
 sign-efi-sig-list -g "$(< GUID.txt)" -k PK.key -c PK.crt KEK KEK.esl KEK.auth
 
 # Signature Database key:
 openssl req -newkey rsa:4096 -nodes -keyout db.key -new -x509 -sha256 -days 365242 -subj "/CN=Signature Database key/" -out db.crt
-openssl x509 -outform DER -in db.crt -out db.cer
 cert-to-efi-sig-list -g "$(< GUID.txt)" db.crt db.esl
 sign-efi-sig-list -g "$(< GUID.txt)" -k KEK.key -c KEK.crt db db.esl db.auth
